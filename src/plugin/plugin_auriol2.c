adcb976901b567e214df5fe47fa7c916774114bb
Batilan <git@atilas.nl>
2014-12-16 14:05:23 +0100

    Improved Auriol2

--- src/plugin/plugin_auriol2.c
+++ src/plugin/plugin_auriol2.c
@@ -55,9 +55,11 @@ r_device auriol2 = {
     /* .id             = */ 101,
     /* .name           = */ "Auriol",
     /* .modulation     = */ OOK_PWM_D,
-    /* .short_limit    = */ 620,
-    /* .long_limit     = */ 1180,
-    /* .reset_limit    = */ 9525,
+//    /* .short_limit    = */ 620,
+//    /* .long_limit     = */ 1180,
+    /* .short_limit    = */ 600,
+    /* .long_limit     = */ 1450,
+    /* .reset_limit    = */ 8000,
     /* .json_callback  = */ &auriol2_callback,
 };
 
@@ -90,20 +92,46 @@ rtl_433_plugin_t plugin =
 /* TODO: put your callback function here, remove the template below, or adjust it */
 static int auriol2_callback(uint8_t bb[BITBUF_ROWS][BITBUF_COLS]) {
     int row, col;
+
+    /*fprintf(stderr, "Called callback plugin for App: %s, type: %s, model: %s, version: %d\n",
+        plugin.plugin_desc.application,
+        plugin.plugin_desc.type,
+        plugin.plugin_desc.model,
+        plugin.plugin_desc.version ); */
+    // TODO: Remove for debugging only
+    for( row = 0; row < 0; row++) {
+  
+        for(col = 0; col < 6; col+=2 ) {
+            fprintf( stderr, "%x%x ", bb[row][col] & 0xff, bb[row][col+1] & 0xff );
+        }
+
+        fprintf( stderr, " - ");
+
+        for(col = 0; col < 8; col++ ) {
+            fprintf( stderr, "%4d", bb[row][col] & 0xff );
+        }
+        fprintf( stderr, " - ");
+        for(col = 0; col < 8; col++ ) {
+            // Print MSB and LSB nibbles
+            fprintf( stderr, "%3d%3d", (bb[row][col] & 0xf0)>>4, (bb[row][col] & 0x0f) );
+        }
+        fprintf( stderr, "\n");
+    }
 // MYTODO remove or replace by debug output call
 // NOTE: Working on this decoder
-    if ( *(int*)bb[0] == 0 && *(int*)bb[2] == 0 && *(int*)bb[1] != 0 && bb[1][5] == 0 &&  bb[1][5] == 0 )
+    if ( *(long*)bb[0] == 0L && *(long*)bb[2] == 0L && *(long*)&(bb[1][4]) != 0L && bb[1][5] == 0 &&  bb[1][6] == 0 &&  bb[1][7] == 0 &&
+         memcmp( bb[1], bb[3], 8) == 0 &&  memcmp( bb[1], bb[5], 8) == 0 &&  memcmp( bb[1], bb[11], 8) == 0 )
     {
         uint8_t nibble[10];
         int hygro;
-        int temp;
+        int tempfx10;
+        float temp;
         int channel;
+        time_t timer;
+        char buffer[25];
+        struct tm* tm_info;
 
-        fprintf(stderr, "Called callback plugin for App: %s, type: %s, model: %s, version: %d\n",
-            plugin.plugin_desc.application,
-            plugin.plugin_desc.type,
-            plugin.plugin_desc.model,
-            plugin.plugin_desc.version );
+
 
         // Split data in Nibbles for easy access
         for (col =0; col < 5; col++)
@@ -112,33 +140,40 @@ static int auriol2_callback(uint8_t bb[BITBUF_ROWS][BITBUF_COLS]) {
             nibble[(col*2)+1] = (bb[1][col] & 0x0f);
         }
 
-        temp = nibble[5]*16 + nibble[6] + 380;
+        tempfx10 = bb[1][2]*16 + nibble[6] - 900;
+        temp = ((tempfx10 / 10.0)-32.0) / 1.8;
         hygro = nibble[7]*10 + nibble[8];
         channel = nibble[9];
 
-        fprintf(stderr, "temp: %d, hygro: %d, channel: %d\n", temp, hygro, channel);
+        /* Determine current time and put in a formatted string buffer */
+        time(&timer);
+        tm_info = localtime(&timer);
+        strftime(buffer, 25, "%Y-%m-%d %H:%M:%S", tm_info);
 
-        // TODO: Remove for debugging only
-        for( row = 0; row < 0; row++) {
-      
-  
-            for(col = 0; col < 8; col+=2 ) {
-                fprintf( stderr, "%x%x ", bb[row][col] & 0xff, bb[row][col+1] & 0xff );
-            }
-    
-            fprintf( stderr, " - ");
+        //fprintf(stderr, "temp: %2.1f, hygro: %d, channel: %d, time = %s\n", temp, hygro, channel, buffer);
+        fprintf(stderr, "temp: %2.1f, hygro: %d, channel: %d, time = %s\n", temp, hygro, channel, buffer);
 
-            for(col = 0; col < 8; col++ ) {
-                fprintf( stderr, "%4d", bb[row][col] & 0xff );
-            }
-            fprintf( stderr, " - ");
-            for(col = 0; col < 8; col++ ) {
-                // Print MSB and LSB nibbles
-                fprintf( stderr, "%3d%3d", (bb[row][col] & 0xf0)>>4, (bb[row][col] & 0x0f) );
-            }
-            fprintf( stderr, "\n");
-        }
         return 1;
+    }
+    else
+    {
+        // TODO: Remove, only for debugging
+        if ( *(long*)bb[0] != 0L  || *(long*)bb[2] != 0L )
+        {
+            printf("Invalid pre-amble %ld %ld\n", *(long*)bb[0],  *(long*)bb[2]);
+        }
+        if ( *(long*)&(bb[1][4]) != 0L )
+        {
+            printf("No data found where expected (5 th byte)\n");
+        }
+        if (  bb[1][5] != 0 ||  bb[1][6] != 0 ||  bb[1][7] != 0 )
+        {
+            printf("Frame too long (%d %d %d)\n", bb[1][5], bb[1][6], bb[1][7]);
+        }
+        if (  memcmp( bb[1], bb[3], 8) != 0 ||  memcmp( bb[1], bb[5], 8) != 0 ||  memcmp( bb[1], bb[11], 8) != 0 )
+        {
+            printf("Frames not identical\n");
+        }
     }
     return 0;
 }
